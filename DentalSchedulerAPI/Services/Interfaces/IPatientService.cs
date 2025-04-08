10cd3757a959733047129dac62eaf3b3d7b21946
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-04-08 15:54:30 -0300

    feat: implement services interfaces

---
+++ DentalSchedulerAPI/Services/Interfaces/IPatientService.cs
@@ -0,0 +1,12 @@
+﻿using DentalSchedulerAPI.DTOs;
+
+namespace DentalSchedulerAPI.Services.Interfaces;
+
+public interface IPatientService
+{
+    Task<IEnumerable<PatientDto>> GetAllAsync();
+    Task<PatientDto?> GetByIdAsync(Guid id);
+    Task<PatientDto> CreateAsync(CreatePatientDto dto);
+    Task<PatientDto?> UpdateAsync(Guid id, CreatePatientDto dto);
+    Task<PatientDto?> DeleteAsync(Guid id);
+}
