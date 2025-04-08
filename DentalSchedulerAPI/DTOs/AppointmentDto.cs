0c83e00074c5c539d0095311edd22b4dd70001df
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-04-08 15:53:34 -0300

    feat: add DTOs for Appointment

--- DentalSchedulerAPI/DTOs/AppointmentDto.cs
+++ DentalSchedulerAPI/DTOs/AppointmentDto.cs
@@ -1,6 +1,11 @@
-﻿namespace DentalSchedulerAPI.DTOs
+﻿namespace DentalSchedulerAPI.DTOs;
+
+public class AppointmentDto
 {
-    public class AppointmentDto
-    {
-    }
+    public Guid Id { get; set; }
+    public Guid patientId { get; set; }
+    public string? PatientName { get; set; }
+    public Guid DentistId { get; set; }
+    public string? DentistName { get; set; }
+    public string Status { get; set; } = "Agendado";
 }
---
+++ DentalSchedulerAPI/DTOs/CreateAppointmentDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DentalSchedulerAPI.DTOs;
+
+public class CreateAppointmentDto
+{
+    [Required]
+    public Guid PatientId { get; set; }
+    [Required]
+    public Guid DentistId { get; set; }
+    public string Status { get; set; } = "Agendado";
+}
