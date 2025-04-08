abbb5c5e0c7c64901706107b8ac77c4cb9dfb9ac
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-04-08 15:56:03 -0300

    feat: create PatientController with CRUD endpoints

---
+++ DentalSchedulerAPI/Controllers/PatientController.cs
@@ -0,0 +1,61 @@
+﻿using DentalSchedulerAPI.DTOs;
+using DentalSchedulerAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalSchedulerAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PatientController : ControllerBase
+{
+    private readonly IPatientService _patientService;
+
+    public PatientController(IPatientService patientService)
+    {
+        _patientService = patientService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PatientDto>>> GetAll()
+    {
+        var patients = await _patientService.GetAllAsync();
+        return Ok(patients);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PatientDto>> GetById(Guid id)
+    {
+        var patient = await _patientService.GetByIdAsync(id);
+        if (patient == null)
+            return NotFound();
+
+        return Ok(patient);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(CreatePatientDto dto)
+    {
+        var newPatient = await _patientService.CreateAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = newPatient.Id }, newPatient);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Update(Guid id, CreatePatientDto dto)
+    {
+        var updatedPatient = await _patientService.UpdateAsync(id, dto);
+        if (updatedPatient == null)
+            return NotFound();
+
+        return Ok(updatedPatient);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var deletedPatient = await _patientService.DeleteAsync(id);
+        if (deletedPatient == null)
+            return NotFound();
+
+        return Ok(deletedPatient);
+    }
+}
--- DentalSchedulerAPI/Program.cs
+++ DentalSchedulerAPI/Program.cs
@@ -1,6 +1,12 @@
 using DentalSchedulerAPI.Data;
+using DentalSchedulerAPI.Mappings;
+using DentalSchedulerAPI.Repositories.Implementations;
+using DentalSchedulerAPI.Repositories.Interfaces;
+using DentalSchedulerAPI.Services;
+using DentalSchedulerAPI.Services.Interfaces;
 using DentalSchedulerAPI.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Internal;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +16,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IDentistRepository, DentistRepository>();
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+
 
 var app = builder.Build();
 
