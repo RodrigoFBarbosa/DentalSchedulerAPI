ca8191995443cf9f8aa49ad33fc931a01e7d5a7f
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-04-08 15:54:57 -0300

    chore: add AutoMapper

---
+++ DentalSchedulerAPI/Mappings/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DentalSchedulerAPI.DTOs;
+using DentalSchedulerAPI.Models;
+
+namespace DentalSchedulerAPI.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        //Patient
+        CreateMap<Patient, PatientDto>();
+        CreateMap<CreatePatientDto, Patient>();
+        CreateMap<CreatePatientDto, Patient>().ReverseMap();
+
+        //Dentist
+        CreateMap<Dentist, DentistDto>();
+        CreateMap<CreateDentistDto, Dentist>();
+        CreateMap<CreateDentistDto, Dentist>().ReverseMap();
+
+        // Appointment
+        CreateMap<Appointment, AppointmentDto>()
+            .ForMember(dest => dest.PatientName, opt => opt.MapFrom(src => src.Patient!.Name))
+            .ForMember(dest => dest.DentistName, opt => opt.MapFrom(src => src.Dentist!.Name));
+
+        CreateMap<CreateAppointmentDto, Appointment>();
+    }
+    
+
+}
