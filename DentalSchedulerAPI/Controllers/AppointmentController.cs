2a082dac1bfe4e000f17c66a1f95ae552e9b1d71
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-04-09 15:54:12 -0300

    feat: add controllers to Appointment and Dentist

---
+++ DentalSchedulerAPI/Controllers/AppointmentController.cs
@@ -0,0 +1,71 @@
+﻿using DentalSchedulerAPI.DTOs;
+using DentalSchedulerAPI.Services.Implementations;
+using DentalSchedulerAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalSchedulerAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentController : ControllerBase
+{
+    private readonly IAppointmentService _appointmentService;
+
+    public AppointmentController(IAppointmentService appointmentService)
+    {
+        _appointmentService = appointmentService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AppointmentDto>>> GetAllAsync()
+    {
+        var appointments = await _appointmentService.GetAllAsync();
+        return Ok(appointments);
+    }
+
+    [HttpGet("{id}", Name = "GetAppointmentById")]
+    public async Task<ActionResult<AppointmentDto>> GetByIdAsync(Guid id)
+    {
+        var appointment = await _appointmentService.GetByIdAsync(id);
+        if (appointment == null)
+            return NotFound();
+
+        return Ok(appointment);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AppointmentDto>> CreateAsync(CreateAppointmentDto dto)
+    {
+        var createdAppointment = await _appointmentService.CreateAsync(dto);
+
+        if (createdAppointment == null)
+            return Conflict("Este horário já está ocupado para o dentista selecionado.");
+
+        return CreatedAtRoute(
+            routeName: "GetAppointmentById",
+            routeValues: new { id = createdAppointment.Id },
+            value: createdAppointment
+);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<AppointmentDto>> UpdateAsync(Guid id, [FromBody] CreateAppointmentDto dto)
+    {
+        var updatedAppointment = await _appointmentService.UpdateAsync(id, dto);
+        if (updatedAppointment == null)
+            return NotFound();
+
+        return Ok(updatedAppointment);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<AppointmentDto>> DeleteAsync(Guid id)
+    {
+        var deletedAppointment = await _appointmentService.DeleteAsync(id);
+        if (deletedAppointment == null)
+            return NotFound();
+
+        return Ok(deletedAppointment);
+    }
+}
---
+++ DentalSchedulerAPI/Controllers/DentistController.cs
@@ -0,0 +1,62 @@
+﻿using DentalSchedulerAPI.DTOs;
+using DentalSchedulerAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalSchedulerAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DentistController : ControllerBase
+{
+    private readonly IDentistService _dentistService;
+
+    public DentistController(IDentistService dentistService)
+    {
+        _dentistService = dentistService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DentistDto>>> GetAll()
+    {
+        var dentists = await _dentistService.GetAllAsync();
+        return Ok(dentists);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<DentistDto>> GetById(Guid id)
+    {
+        var dentist = await _dentistService.GetByIdAsync(id);
+        if (dentist is null) 
+            return NotFound();
+
+        return Ok(dentist);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<DentistDto>> Create(CreateDentistDto dto)
+    {
+        var newDentist = await _dentistService.CreateAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = newDentist.Id }, newDentist);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<DentistDto>> Update(Guid id, CreateDentistDto dto)
+    {
+        var updatedDentist = await _dentistService.UpdateAsync(id, dto);
+        if (updatedDentist is null)
+            return NotFound();
+
+        return Ok(updatedDentist);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<DentistDto>> Delete(Guid id)
+    {
+        var deletedDentist = await _dentistService.DeleteAsync(id);
+        if (deletedDentist is null) 
+            return NotFound();
+
+        return Ok(deletedDentist);
+    }
+}
