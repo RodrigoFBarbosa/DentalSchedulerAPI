380b764a8b0abe5ca8f70f4d914ce991c655d89a
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-04-10 16:41:59 -0300

    feat: add validators to Program

--- DentalSchedulerAPI/Program.cs
+++ DentalSchedulerAPI/Program.cs
@@ -5,6 +5,9 @@ using DentalSchedulerAPI.Repositories.Interfaces;
 using DentalSchedulerAPI.Services.Implementations;
 using DentalSchedulerAPI.Services.Interfaces;
 using DentalSchedulerAPI.UnitOfWork;
+using DentalSchedulerAPI.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Internal;
 
@@ -13,10 +16,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateAppointmentDtoValidator>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -39,6 +45,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<DentalSchedulerAPI.Middlewares.ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
